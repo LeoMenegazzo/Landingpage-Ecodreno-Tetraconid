01b16721d008363e77178794b386110f848813a2
Leonardo Ari Santos Menegazzo <leonardoarimenegazzo@gmail.com>
2022-03-16 11:03:48 -0400

    Ecodreno

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,36 +1,77 @@
-// Select all links with hashes
-$('a[href*="#"].scroll')
-// Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-// On-page links
-if (
-location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-&& 
-location.hostname == this.hostname
-) {
-// Figure out element to scroll to
-var target = $(this.hash);
-target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-// Does a scroll target exist?
-if (target.length) {
-// Only prevent default if animation is actually gonna happen
-event.preventDefault();
-$('html, body').animate({
-scrollTop: target.offset().top
-}, 1000, function() {
-// Callback after animation
-// Must change focus!
-var $target = $(target);
-$target.focus();
-if ($target.is(":focus")) { // Checking if the target was focused
-return false;
-} else {
-$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-$target.focus(); // Set focus again
-};
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 110) {
+            $(".wq-header").each(function () {
+                $(this).addClass("wq-header_fixo");
+            });
+        } else {
+            $(".wq-header").each(function () {
+                $(this).removeClass("wq-header_fixo");
+            });
+        }
+    });
+    $(window).scroll();
+    
+    $('a[href*="#"].scroll, a[href*="#"].scroll2')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    event.preventDefault();
+                    $("html, body").animate({ scrollTop: target.offset().top - 125 }, 1000, function () {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr("tabindex", "-1");
+                            $target.focus();
+                        }
+                    });
+                }
+            }
+        });
 });
-}
-}
-});
+var hrefScroll;
+
+$('.wq-instalacao-carousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
